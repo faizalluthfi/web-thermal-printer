507cd569a118bf87f7c2a3fb06299f996210b425
Faizal Luthfi <zalluth@gmail.com>
2020-01-25 21:00:56 +0700

    load generated html

---
+++ main.js
@@ -0,0 +1,15 @@
+const { app, BrowserWindow } = require('electron');
+
+let win;
+
+app.on('ready', () => {
+  win = new BrowserWindow();
+  win.loadFile('html/index.html');
+});
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') app.quit();
+});
